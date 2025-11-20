56e7b2ef92f6e405b90f6ecff142e65b5a48c001
unknown <laura.hellenbroich@netcologne.de>
2025-11-20 17:18:26 +0100

    h2 Verbessert

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,12 +3,16 @@ package h2;
 public class H2_main {
 	public static void main(String[] args) {
 		int n=576565, digits=10;
-		int[] a= {0,0,0,0,0,0,0,0,0};
+		int[] a= new int[9];
 		
 		String s= String.valueOf(n);
 		digits=s.length();
 		
 		System.out.println(digits);
+		
+		for (int x=0; x<9;x++) {
+			a[x]=0;
+		}
 		
 		for (int i=8; i>8-digits; i--) {
 			a[i]=n%10;
